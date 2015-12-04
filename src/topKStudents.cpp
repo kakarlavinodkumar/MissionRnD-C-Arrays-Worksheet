2100a48ad08b6453e4985a1d5c50070070e03e2b
kakarlavinodkumar <vinodkakarla564@gmail.com>
2015-12-03 19:11:14 -0800

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -20,7 +20,30 @@ struct student {
 	char *name;
 	int score;
 };
-
 struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+	int index1, index2;
+	struct student **topk;
+	if (students == NULL || len<0||K<=0)
+		return NULL;
+	if (K>len)
+		K = len;
+	for (index1 = 0; index1 < len; index1++)
+	{
+		for (index2 = 0; index2 < len - 1; index2++)
+		{
+			if (students[index2].score > students[index2 + 1].score)
+			{
+				students[index2].score = students[index2].score + students[index2 + 1].score;
+				students[index2 + 1].score = students[index2].score - students[index2 + 1].score;
+				students[index2].score = students[index2].score - students[index2 + 1].score;
+			}
+		}
+	}
+	topk = (struct student **)malloc(sizeof(struct student*)*K);
+	for (index1 = 0; index1 < K; index1++)
+	{
+		topk[index1] = (struct student*)malloc(sizeof(struct student));
+		topk[index1]->score = students[len - K + index1].score;
+	}
+	return topk;
 }
