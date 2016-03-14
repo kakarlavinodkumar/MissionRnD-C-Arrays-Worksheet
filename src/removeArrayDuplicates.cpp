b9b927774b3ec2377aeaf7333ecb55bd858ff45e
kakarlavinodkumar <vinodkakarla564@gmail.com>
2016-03-14 06:57:15 -0700

    Update removeArrayDuplicates.cpp

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -12,21 +12,55 @@ NOTES: Don't create new array, try to change the input array.
 */
 
 #include <stdio.h>
-
+struct binary_search_tree
+{
+	int data;
+	struct binary_search_tree *left, *right;
+}*root,*current,*newnode,*temp;
 void * removeArrayDuplicates(int *Arr, int len)
 {
-	int index1, index2, count;
+	int index1, count;
+	bool flag;
 	if (Arr==NULL||len<0)
 	return NULL;
-	for (index1 = count = 0; index1 < len - 1; index1++)
+	root = (struct binary_search_tree*)malloc(sizeof(binary_search_tree));
+	root->left = root->right = NULL;
+	root->data = Arr[0];
+	for (index1 = count = 1; index1 < len ; index1++)
 	{
-		for (index2 = index1 + 1; index2 < len; index2++)
+		for (current = root,flag=0; current != NULL;)
 		{
-			if (Arr[index1] == Arr[index2])
+			if (current->data == Arr[index1])
 				break;
+			if (current->data < Arr[index1])
+			{
+				if (current->right == NULL)
+				{
+					flag = 1;
+					break;
+				}
+				current = current->right;
+			}
+			else
+			{
+				if (current->left == NULL)
+				{
+					flag = 1;
+					break;
+				}
+				current = current->left;
+			}
 		}
-		if (index2 == len)
-			Arr[count++] = Arr[index1];
+			if (flag==1)
+			{
+				newnode = (struct binary_search_tree *)(malloc(sizeof(struct binary_search_tree)));
+				newnode->data = Arr[index1];
+				newnode->left = newnode->right = NULL;
+				if (current->data < newnode->data)
+					current->right = newnode;
+				else
+					current->left = newnode;
+				Arr[count++] = Arr[index1];
+			}
 	}
-	Arr[count] = Arr[index1];
 }
